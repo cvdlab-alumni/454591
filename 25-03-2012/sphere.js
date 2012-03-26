30c45360a057d8fa247bdac494fbaff3161c4f78
Roberto <dilallo.roberto@gmail.com>
2012-03-26 21:33:06 +0200

    aggiunta di sphere

---
+++ 25-03-2012/sphere.js
@@ -0,0 +1,19 @@
+var drawSphere = function(r,n){
+	var d = DOMAIN([[0,PI],[0,2*PI]])([n,2*n]);
+
+	var mapping = function (p){
+		var alfa = p[0]-(PI/2);
+		var beta = p[1];
+
+		var x = r*COS(alfa)*COS(beta);
+		var y = r*COS(alfa)*SIN(beta);
+		var z = r*SIN(alfa);
+
+		return [x,y,z];
+	}
+	var mapped = MAP(mapping)(d);
+
+
+	DRAW(mapped);
+	return mapped;
+}
